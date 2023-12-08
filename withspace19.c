747965a4e9c74502bb2cab939de5965604ed55d0
krishnasorathiya167 <142407148+krishnasorathiya167@users.noreply.github.com>
2023-12-08 18:41:42 +0530

    Add files via upload

---
+++ withspace19.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+main()
+{
+	int i,j,k;
+	
+	for(i=1;i<=5;i++)
+	{
+		for(k=i;k>1;k--)
+		{
+			printf(" ");	
+		}
+		for(j=i;j<=5;j++)
+		{
+			if(j%2==1)
+			{
+				printf("1");
+			}
+			else
+			{
+				printf("0");
+			}
+		}
+		printf("\n");
+	}
+}
